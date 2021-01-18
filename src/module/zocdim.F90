e58d606d99fbf4417822887e7c13c76b95c9797e
Takao Kawasaki <kawasaki@aori.u-tokyo.ac.jp>
2021-01-18 17:52:04 +0900

    add src/option/dvdif.gls.ted.F90

--- src/module/zocdim.F90
+++ src/module/zocdim.F90
@@ -4,22 +4,21 @@ module zocdim
  
   implicit none
  
-  integer, parameter :: nxg    =     32, nyg    =     32, nz     =     28
-  integer, parameter :: kz     =      5
+  integer, parameter :: nxg    =    360, nyg    =    256, nz     =     63
+  integer, parameter :: kz     =     10
   integer, parameter :: nic    =      5
-  integer, parameter :: nxgdim =     36, nygdim =     36, nzdim  =     30
+  integer, parameter :: nxgdim =    364, nygdim =    260, nzdim  =     65
   integer, parameter :: igstr  =      3, jgstr  =      3, kstr   =      2
-  integer, parameter :: igend  =     34, jgend  =     34, kend   =     29
-  integer, parameter :: ijgstr =     75, ijgend =   1222
+  integer, parameter :: igend  =    362, jgend  =    258, kend   =     64
  
   integer, parameter :: nxyg = nxg*nyg, nxyzg = nxyg*nz
   integer, parameter :: nxygdm = nxgdim*nygdim, nxyzgd = nxygdm*nzdim
  
-  integer, parameter :: nx     =     16, ny     =     16
-  integer, parameter :: nxdim  =     20, nydim  =     20
+  integer, parameter :: nx     =     10, ny     =      8
+  integer, parameter :: nxdim  =     14, nydim  =     12
   integer, parameter :: istr   =      3, jstr   =      3
-  integer, parameter :: iend   =     18, jend   =     18
-  integer, parameter :: ijstr  =     43, ijend  =    358
+  integer, parameter :: iend   =     12, jend   =     10
+  integer, parameter :: ijstr  =     31, ijend  =    138
  
   integer, parameter :: ntdim =      2
  
@@ -40,11 +39,9 @@ module zocdim
   integer, parameter :: lse = -nxdim+1, lsw = -nxdim-1
   integer, parameter :: lww = -2, lss = -2 * nxdim
  
-  integer, parameter :: inodes =      2, jnodes =      2
+  integer, parameter :: inodes =     36, jnodes =     32
   integer, parameter :: icomm = istr-1, jcomm = jstr-1
  
-  integer, parameter :: ijtgst =    112, ijtged =   1185
-  integer, parameter :: ijvgst =    112, ijvged =   1148
  
   logical, save :: oinit, ofinal
  
