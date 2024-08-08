6a184b25abbb1481ca6bbe12c5818510c34a9353
Takao KAWASAKI <kawasaki@aori.u-tokyo.ac.jp>
2024-08-08 20:18:06 +0900

    mv option/{tslvt,aprdc}.icedcoco.tundf.F90 depend.icedcoco/{tslvt,aprdc}.F90

--- src/depend.icedcoco/aprdc.F90
+++ src/depend.icedcoco/aprdc.F90
@@ -70,6 +70,7 @@ contains
     use brstt
     use bchmk
     use qckot
+    use ucaln
     use bshft
 
     implicit none
@@ -413,9 +414,9 @@ contains
     &                nxdim,  nydim,      1)
 #endif
           call shdiff(   tx,     hx)
-!          call clcstr('TUNDIF')
-!          call tundif(   tx,     hx)
-!          call clcend('TUNDIF')
+          call clcstr('TUNDIF')
+          call tundif(   tx,     hx)
+          call clcend('TUNDIF')
           call ovturn(    r,     tx,     hx)
 #ifdef OPT_BBL
           call stbbtr(   tx   )
