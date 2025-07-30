5ffef98545447c0a550ef2606ea45729e38b9e27
Masao Kurogi <m_kurogi@jamstec.go.jp>
2025-07-30 12:48:59 +0900

    M src/option/tflxt.ut.F90: To prevent referring uninitialized variables, do loop range (L732) is changed.

--- src/option/tflxt.ut.F90
+++ src/option/tflxt.ut.F90
@@ -729,7 +729,7 @@ subroutine flxtrc(  &
   end do
 
   do k = kstr, kend
-     do ij=1, nxydim
+     do ij = ijtstr, ijtend+nxdim
         igsx(ij, k) = ( ahi3d(ij, k) - ahg3d(ij, k) ) * xdzdx(ij, k)
         igsy(ij, k) = ( ahi3d(ij, k) - ahg3d(ij, k) ) * ydzdy(ij, k)
      end do
